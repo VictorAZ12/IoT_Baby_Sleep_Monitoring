fe87e8d128b6a91911e2ea8954a4f5959f763141
FinnMurphy98 <murphy.finn@hotmail.com>
2022-09-19 13:56:07 +0800

    added more scaffolding and templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
 from app import app
+from flask import render_template, redirect
+from datetime import datetime
+from app.config import TIMEZONE
+
+database = {'temperature': '25˚C', 'time': datetime.now(tz=TIMEZONE)}
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return "Hello World!"
+    message = 'Welcome to Baby Sleep Monitor'
+    return render_template('index.html', message=message)
+
+@app.route('/dashboard')
+def dashboard():
+    data = database['temperature']
+    return render_template('dashboard.html', data=data)
+
+@app.route('/update/temperature/<data>')
+def update_temperature(data):
+    global database; 
+    database['temperature'] = data + '˚C'
+    database['time'] = datetime.now(tz=TIMEZONE)
+    return redirect("/dashboard")
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,13 +3,33 @@ import unittest
 
 class TestIndex(unittest.TestCase):
 
-    def test_status_code(self):
+    def test_status(self):
         """
-        Check the response status code is 200 OK. 
+        Check the http response status code is 200 OK. 
         """
         client = app.test_client(self)
         response = client.get('/index', content_type='html/text')
         self.assertEqual(response.status_code, 200)
+
+class TestDashboard(unittest.TestCase):
+
+    def test_status(self):
+        """
+        Check the http response status code is 200 OK. 
+        """
+        client = app.test_client(self)
+        response = client.get('/dashboard', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+class TestUpdateTemperature(unittest.TestCase):
+
+    def test_status(self):
+        """
+        Check the http response status code is 302 Found.
+        """
+        client = app.test_client(self)
+        response = client.get('/update/temperature/25', content_type='html/text')
+        self.assertEqual(response.status_code, 302)
 
 if __name__ == '__main__':
     unittest.main()
