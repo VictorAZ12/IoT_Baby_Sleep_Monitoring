540838a12adaa5e88c00197dc134b23fac3d02e3
FinnMurphy98 <murphy.finn@hotmail.com>
2022-10-14 19:14:16 +0800

    added some comments to functions

--- app/routes.py
+++ app/routes.py
@@ -17,7 +17,7 @@ def index():
 @app.route('/dashboard')
 def dashboard():
     """
-    Displays the most recent record of data for the dashboard page.
+    Displays the 20 most recent record of data for the dashboard page.
     """
     records = iotDB.select_latest('iotDB.db', 20, 1)
     data = []
@@ -54,12 +54,18 @@ def update(sound, movement, humidity, temperature, device_id):
 def report(yr, mon, day, hr, min, length):
     """
     Returns a page that displays useful information about the data over the selected period of time. 
+      - Displays the selected time period over which the report is to be made. 
+      - Noise ratio: a number between 0 and 1 measuring how noisy the time period was
+      - Motion ratio: " "
+      - Average temperature and average humidity
+      - Sleep quality rating, and aggregate of the other 4 quantities. 
     @param yr (str): the year to start the report period.
     @param mon (str): the month to start the report period.
     @param day (str): the day to start the report period.
     @param hr (str): the hour to start the repord period (24hr time)
     @param min (str): the minute to start the report period.
     @param length (str): the number of hours the report should last for.
+    @return report.html page displaying the passed arguments.
     """
     begin = datetime(year=int(yr), month=int(mon), day=int(day), hour=int(hr), minute=int(min))
     stop = begin + timedelta(hours=float(length))
@@ -80,13 +86,9 @@ def report(yr, mon, day, hr, min, length):
         humidity_count += int(record[4])
         temp_count += int(record[5])
     sound_ratio = round(sound_count / num_records, 2)
-    #messages.append(sound_ratio)
     motion_ratio = round(motion_count / num_records, 2)
-    #messages.append(motion_ratio)
     average_humidity = int(humidity_count / num_records)
-    #messages.append(average_humidity)
     average_temp = int(temp_count / num_records)
-    #messages.append(average_temp)
     sleep_quality = 0
     sleep_quality += 1 - sound_ratio
     sleep_quality += 1 - motion_ratio
@@ -104,31 +106,4 @@ def report(yr, mon, day, hr, min, length):
         sleep_quality += 1
     sleep_quality = int((sleep_quality / 4) * 100)
     messages.extend([sound_ratio, motion_ratio, average_humidity, average_temp, sleep_quality])
-    # messages.append(sleep_quality)
     return render_template('report.html', dates=[begin[0:19], stop[0:19]], messages=messages)
-
-
-def dashboard_v2():
-    """
-    UNFINISHED.
-    """
-    # records = iotDB.select_latest('iotDB.db', 20, 1)
-    now = datetime.now(tz=TIMEZONE)
-    past = now - timedelta(hours=1)
-    now = now.strftime("%Y-%m-%d %H:%M:%S.%f")
-    past = past.strftime("%Y-%m-%d %H:%M:%S.%f")
-    records = iotDB.select_range('iotDB.db', start=past, end=now, mode=1)
-    data = []
-    counter = 1
-    for record in records:
-        if counter % 36 == 0 or counter == 1:
-            data_element = {
-                    "time": record[1][11:19],
-                    "sound": record[2],
-                    "movement": record[3],
-                    "humidity": record[4],
-                    "temperature": record[5]
-            }
-        data.append(data_element)
-        counter += 1
-    return jsonify(data)
